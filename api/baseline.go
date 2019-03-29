0071f43c7017d3fcbaf338cf72d468987e81a8ba
ThatMomentIsOver <chesky@vivaldi.net>
2019-03-29 12:31:29 +0800

    fix baseline bug

--- api/baseline.go
+++ api/baseline.go
@@ -3,11 +3,18 @@ package api
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
+	"os"
 	"strings"
 )
 
 func CheckSSH() {
 	filePath := fmt.Sprintf("imagesTemp/%s/layer/etc/rc.local", TopLayerID)
+	fmt.Println(filePath)
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		log.Println("ssh config file not exist")
+		return
+	}
 	r, err := ioutil.ReadFile(filePath)
 	errorPanic(err)
 	if strings.Contains(string(r), "start") {
